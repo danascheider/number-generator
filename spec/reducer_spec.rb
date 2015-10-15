482de4e784f855f68bb035a593c1c4cc26500c58
Dana Scheider <dana.scheider@gmail.com>
2015-10-14 20:41:01 -0700

    Change API for Reducer class similar to how I did for the SumGenerator class

--- spec/reducer_spec.rb
+++ spec/reducer_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
 
 describe Reducer do 
-  let(:reducer) { Reducer.new([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) }
 
-  describe 'reduce' do
+  describe 'Reducer::reduce' do
     it 'returns an array of only the primes' do 
-      expect(reducer.reduce).to eql([2, 3, 5, 7])
+      expect(Reducer.reduce([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to eql([2, 3, 5, 7])
     end
   end
 end
