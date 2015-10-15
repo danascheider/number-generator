ce8277339b5cce033552f936f4dcf56d672ce84d
Dana Scheider <dana.scheider@gmail.com>
2015-10-14 20:43:30 -0700

    Delete empty line

--- spec/reducer_spec.rb
+++ spec/reducer_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Reducer do 
-
   describe 'Reducer::reduce' do
     it 'returns an array of only the primes' do 
       expect(Reducer.reduce([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to eql([2, 3, 5, 7])
