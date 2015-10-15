0eb3b2681e20db2e900a7d2787fb4a2e249fee9a
Dana Scheider <dana.scheider@gmail.com>
2015-10-14 20:45:58 -0700

    Use splat args instead of requiring an input array

--- lib/sum_generator.rb
+++ lib/sum_generator.rb
@@ -2,8 +2,8 @@ require 'prime'
 
 class SumGenerator
   class << self
-    def generate_sums(array)
-      array.map {|num| num.to_s.split('').map(&:to_i).inject(&:+) }
+    def generate_sums(*args)
+      args.map {|num| num.to_s.split('').map(&:to_i).inject(&:+) }
     end
   end
 end
