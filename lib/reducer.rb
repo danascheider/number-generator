d4676841e1cda826a18cc0d296bb3087e2d77477
Dana Scheider <dana.scheider@gmail.com>
2015-10-14 20:47:05 -0700

    Change the reducer also to take splat args instead of a regular array

--- lib/reducer.rb
+++ lib/reducer.rb
@@ -1,7 +1,7 @@
 class Reducer
   class << self
-    def reduce(array)
-      array.reject {|n| !Prime.prime?(n) }
+    def reduce(*args)
+      args.reject {|n| !Prime.prime?(n) }
     end
   end
 end
